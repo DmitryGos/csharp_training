cfaa3d15ad348d3a761955e9d07e5c0c73167240
DmitryGos - Work <dmitrygos@gmail.com>
2019-05-22 18:07:49 +0300

    Лекция 3.4. Оптимизация переходов между страницами - Problem

--- addressbook-web-tests/addressbook-web-tests/AppManager/NavigationHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/AppManager/NavigationHelper.cs
@@ -21,14 +21,29 @@ namespace WebAddressbookTests
 
         public void GotoHomePage()
         {
+            if (driver.Url == baseURL + "/addressbook/")
+            {
+                return;
+            }
             driver.Navigate().GoToUrl(baseURL + "/addressbook/");
         }
         public void GotoGroupsPage()
         {
+            if (driver.Url == baseURL + "/addressbook/group.php" && IsElementPresent(By.Name("new")))
+            {
+                return;
+            }
+
             driver.FindElement(By.LinkText("groups")).Click();
         }
         public void GotoContactAddingPage()
         {
+            if (driver.Url == baseURL + "/addressbook/edit.php" 
+                && IsElementPresent(By.Name("submit")))
+            {
+                return;
+            }
+
             driver.FindElement(By.LinkText("add new")).Click();
         }
 
