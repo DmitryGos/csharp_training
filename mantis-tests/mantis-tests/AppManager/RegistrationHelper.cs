1ce23620c48551d80c75e075f18953ab3cb2bc1a
DmitryGos - Work <dmitrygos@gmail.com>
2019-06-29 23:24:52 +0300

    Лекция 9.1. Загрузка файлов на удалённый сервер по протоколу FTP - Done

---
+++ mantis-tests/mantis-tests/AppManager/RegistrationHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace mantis_tests
+{
+    public class RegistrationHelper : HelperBase
+    {
+        public RegistrationHelper(ApplicationManager manager) : base(manager) { }
+
+        public void Register(AccountData account)
+        {
+            OpenMainPage();
+            OpenRegistrationForm();
+            FillRegistrationForm(account);
+            SubmitResgistration();
+        }
+
+        private void OpenRegistrationForm()
+        {
+            driver.FindElement(By.CssSelector("a.back-to-login-link.pull-left")).Click();
+        }
+
+        private void SubmitResgistration()
+        {
+            driver.FindElement(By.CssSelector("input[type='submit']")).Click();
+        }
+
+        private void FillRegistrationForm(AccountData account)
+        {
+            driver.FindElement(By.Id("username")).SendKeys(account.Name);
+            driver.FindElement(By.Id("email-field")).SendKeys(account.Email);
+
+        }
+
+        private void OpenMainPage()
+        {
+            manager.Driver.Url = "http://localhost/mantisbt-2.21.1/login_page.php";
+        }
+    }
+}
---
+++ mantis-tests/mantis-tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("mantis-tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("mantis-tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("519ccd2b-3f34-45b1-b5f1-3101a608e374")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
